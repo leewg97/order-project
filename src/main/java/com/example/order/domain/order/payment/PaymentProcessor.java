05e04eea10bffb2a6ff01b7064e7e11e5a742a4e
leewg97 <leewg97@naver.com>
2023-03-26 20:58:28 +0900

    Feat: Order 의 결제 처리 로직 구현 * paymentProcessor 구현

---
+++ src/main/java/com/example/order/domain/order/payment/PaymentProcessor.java
@@ -0,0 +1,8 @@
+package com.example.order.domain.order.payment;
+
+import com.example.order.domain.order.Order;
+import com.example.order.domain.order.OrderCommand;
+
+public interface PaymentProcessor {
+    void pay(Order order, OrderCommand.PaymentRequest request);
+}
